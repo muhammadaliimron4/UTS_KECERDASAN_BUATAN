17b1dde1b8401ed9ba11f7a6e6e28e5f68a0ccc4
muhammadaliimron4 <104206629+muhammadaliimron4@users.noreply.github.com>
2022-04-22 19:43:46 +0700

    Add files via upload

---
+++ utskecerdasanbuatan.py
@@ -0,0 +1,16 @@
+# Nama : Muhammad ali imron
+# Nim : 191011400308
+# Kelas : 06 TPLM 003 
+
+nilai = 85 print
+
+("Nilai anda adalah:", nilai, "\n") 
+
+if nilai >= 85:
+   print("Selamat, anda lulus! GRADE A")
+elif nilai >= 75: 
+   print("Selamat, anda lulus! GRADE B")
+elif nilai >= 65: 
+   print("Selamat, anda lulus! GRADE C")
+else: 
+   print("Maaf, anda tidak lulus, dikarenakan nilai anda berada dibawah minimum :'(")
